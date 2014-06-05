e49cfb3601e5f4abd72f50222ade539e742342b2
Daniel Blair <CMDann.Development@gmail.com>
2014-06-04 22:08:30 -0500

    Added the install script to create the database table to hold the mapply api and google api keys. Added the functions and the call backs to get the keys from the db. Added functions to save the keys to the db as well

--- mapply.php
+++ mapply.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Mapply
-Plugin URI: http://www.mapply.new/wordpress-app
+Plugin URI: https://github.com/BOLDInnovationGroup/Mapply-WordPress-Plugin
 Description: Dislay a map of your stores on your WordPress site!
 Version: 1.0
 Author: Mapply
@@ -16,6 +16,41 @@ DEFINE("DEMOLP_HEADINGSTART", "<h4>Mapplyß</h4>");
 
 // Shortcodes
 add_shortcode("mapply", "mapply_handler");
+
+// Add actions
+add_action( 'wp_mapply_api', 'get_mapply_api' );
+add_action( 'wp_mapply_gapi', 'get_google_api' );
+
+// Install functions
+register_activation_hook( __FILE__, 'jal_install' );
+
+global $mapply_db_version;
+$mapply_db_version = "1.0";
+
+function jal_install () {
+   global $wpdb;
+
+   $installed_ver = get_option( "mapply_db_version" );
+   $table_name = $wpdb->prefix . "mapply";
+
+  if( $installed_ver != $mapply_db_version ) {
+
+    $sql = "CREATE TABLE $table_name (
+      id mediumint(9) NOT NULL AUTO_INCREMENT,
+      mapply_api VARCHAR(255) DEFAULT '' NOT NULL,
+      google_api VARCHAR(255) DEFAULT '' NOT NULL,
+      UNIQUE KEY id (id)
+    );";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+
+    update_option( "mapply_db_version", $jal_db_version );
+
+  }
+}
+
+// End of Install functions
 
 function mapply_handler($incomingfrompost) {
 
@@ -46,6 +81,31 @@ function demolistposts_function($incomingfromhandler) {
   $demolp_output .= wp_specialchars_decode($incomingfromhandler["listend"]);
   $demolp_output .= wp_specialchars_decode($incomingfromhandler["headingend"]);
   return $demolp_output;
+}
+
+// Save functions for API Keys
+function save_mapply_api($api){
+  global $wpdb;
+
+}
+
+function save_google_api($gapi){
+  global $wpdb;
+
+}
+
+function get_mapply_api(){
+  global $wpdb;
+
+  $api = "";
+  return $api;
+}
+
+function get_google_api(){
+  global $wpdb;
+
+  $gapi = "";
+  return $gapi;
 }
 
 ?>
