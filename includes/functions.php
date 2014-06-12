d94798d7301081e845be6f66fc3489663ec60816
Dann Blair <cmdann@gmail.com>
2014-06-11 20:15:25 -0500

    Moved code to separate files. Fixed redirection issue. Added checkbox to turn off reef link (leave that on though?)

---
+++ includes/functions.php
@@ -0,0 +1,116 @@
+<?php
+
+// Create the row to store the keys
+function create_first_row(){
+  global $wpdb;
+  $table_name = get_table_name();
+  $wpdb->insert( $table_name, array('mapply_api' => '', 'google_api' => '', 'display_refferal' => '1'), array());
+}
+
+// Save the mapply API key
+function save_mapply_api($api){
+  global $wpdb;
+
+  $table_id = 1;
+  $table_name = get_table_name();
+  $wpdb->query($wpdb->prepare("UPDATE ".$table_name." SET mapply_api='$api' WHERE id = %d", $table_id));
+}
+
+// Save the refferal link to mapply
+function save_mapply_link($link){
+  global $wpdb;
+
+  $table_id = 1;
+  $table_name = get_table_name();
+  $wpdb->query($wpdb->prepare("UPDATE ".$table_name." SET mapply_link='$link' WHERE id = %d", $table_id));
+}
+
+// Save the Google API key
+function save_google_api($gapi){
+  global $wpdb;
+
+  $table_id = 1;
+  $table_name = get_table_name();
+  $wpdb->query($wpdb->prepare("UPDATE ".$table_name." SET google_api='$gapi' WHERE id = %d", $table_id));
+}
+
+function save_display_ref($ref){
+  global $wpdb;
+
+  $table_id = 1;
+  $table_name = get_table_name();
+  $wpdb->query($wpdb->prepare("UPDATE ".$table_name." SET display_refferal='$ref' WHERE id = %d", $table_id));
+}
+
+// Get the mapply api from the db
+function get_mapply_api(){
+  global $wpdb;
+
+  $table_id = 1;
+  $table_name = get_table_name();
+  $api = $wpdb->get_row( $wpdb->prepare( "SELECT mapply_api FROM " .$table_name. " WHERE ID = %d", $table_id));
+  return $api->mapply_api;
+}
+
+// Get the google API from the db
+function get_google_api(){
+  global $wpdb;
+
+  $table_id = 1;
+  $table_name = get_table_name();
+  $gapi = $wpdb->get_row( $wpdb->prepare( "SELECT google_api FROM " .$table_name. " WHERE ID = %d", $table_id));
+  return $gapi->google_api;
+}
+
+function get_display_ref(){
+  global $wpdb;
+
+  $table_id = 1;
+  $table_name = get_table_name();
+  $gapi = $wpdb->get_row( $wpdb->prepare( "SELECT display_refferal FROM " .$table_name. " WHERE ID = %d", $table_id));
+  return $gapi->display_refferal;
+}
+
+// Get the refferal link from the database
+function get_mapply_refferal_url(){
+  global $wpdb;
+
+  $table_id = 1;
+  $table_name = get_table_name();
+  $href = $wpdb->get_row( $wpdb->prepare( "SELECT mapply_link FROM " .$table_name. " WHERE ID = %d", $table_id));
+  return $href->mapply_link;
+}
+
+// Process the form data
+function process_mapply_keys(){
+  if ($_POST){
+
+    // Check for the google api key
+    if (isset($_POST['google_api_key'])){
+      save_google_api(sanitize_text_field($_POST['google_api_key']));
+    }
+
+    // Check for the apply api key
+    if (isset($_POST['mapply_api_key'])){
+      save_mapply_api(sanitize_text_field($_POST['mapply_api_key']));
+    }
+
+    // Check if the mapply link was posted
+    if (isset($_POST['mapply_link'])){
+      save_mapply_link($_POST['mapply_link']);
+    }
+
+    if (isset($_POST['display_ref'])){
+        save_display_ref(1);
+    } else {
+        save_display_ref(0);
+    }
+
+    // redirect
+    wp_redirect(admin_url( 'admin.php?page=mapply/mapply.php'));
+    exit;
+  }
+}
+
+
+?>
--- mapply.php
+++ mapply.php
@@ -47,6 +47,7 @@ function mapply_install () {
       mapply_api VARCHAR(255) DEFAULT "" NOT NULL,
       google_api VARCHAR(255) DEFAULT "" NOT NULL,
       mapply_link VARCHAR(255) DEFAULT "",
+      display_refferal INT(1) DEFAULT "1" NOT NULL,
       UNIQUE KEY id (id)
     );';
 
@@ -89,13 +90,18 @@ function build_script_text(){
   $api = get_mapply_api();
   $gapi = get_google_api();
   $mapply_link = get_mapply_refferal_url();
+  $display_ref = get_display_ref();
 
   $script = '<script id="locator" type="text/javascript" src="//app.mapply.net/front-end/js/locator.js" data-api-key="store_locator.';
   $script .= $api;
   $script .= '" data-path="//app.mapply.net/front-end/" data-maps-api-key="';
   $script .= $gapi;
   $script .= '" ></script>';
-  $script .= $mapply_link;
+
+  // If the user has selected display ref link, add it.
+  if ($display_ref == "1"){
+    $script .= $mapply_link;
+  }
 
   return $script;
 }
@@ -119,97 +125,7 @@ function script_output($incomingfromhandler) {
   return $demolp_output;
 }
 
-// Create the row to store the keys
-function create_first_row(){
-  global $wpdb;
-  $table_name = get_table_name();
-  $wpdb->insert( $table_name, array('mapply_api' => '', 'google_api' => ''), array());
-}
-
-// Save the mapply API key
-function save_mapply_api($api){
-  global $wpdb;
-
-  $table_id = 1;
-  $table_name = get_table_name();
-  $wpdb->query($wpdb->prepare("UPDATE ".$table_name." SET mapply_api='$api' WHERE id = %d", $table_id));
-}
-
-// Save the refferal link to mapply
-function save_mapply_link($link){
-  global $wpdb;
-
-  $table_id = 1;
-  $table_name = get_table_name();
-  $wpdb->query($wpdb->prepare("UPDATE ".$table_name." SET mapply_link='$link' WHERE id = %d", $table_id));
-}
-
-// Save the Google API key
-function save_google_api($gapi){
-  global $wpdb;
-
-  $table_id = 1;
-  $table_name = get_table_name();
-  $wpdb->query($wpdb->prepare("UPDATE ".$table_name." SET google_api='$gapi' WHERE id = %d", $table_id));
-}
-
-// Get the mapply api from the db
-function get_mapply_api(){
-  global $wpdb;
-
-  $table_id = 1;
-  $table_name = get_table_name();
-  $api = $wpdb->get_row( $wpdb->prepare( "SELECT mapply_api FROM " .$table_name. " WHERE ID = %d", $table_id));
-  return $api->mapply_api;
-}
-
-// Get the google API from the db
-function get_google_api(){
-  global $wpdb;
-
-  $table_id = 1;
-  $table_name = get_table_name();
-  $gapi = $wpdb->get_row( $wpdb->prepare( "SELECT google_api FROM " .$table_name. " WHERE ID = %d", $table_id));
-  return $gapi->google_api;
-}
-
-// Get the refferal link from the database
-function get_mapply_refferal_url(){
-  global $wpdb;
-
-  $table_id = 1;
-  $table_name = get_table_name();
-  $href = $wpdb->get_row( $wpdb->prepare( "SELECT mapply_link FROM " .$table_name. " WHERE ID = %d", $table_id));
-  return $href->mapply_link;
-}
-
-// Process the form data
-function process_mapply_keys(){
-  if ($_POST){
-
-    // Check for the google api key
-    if (isset($_POST['google_api_key'])){
-      save_google_api(sanitize_text_field($_POST['google_api_key']));
-    }
-
-    // Check for the apply api key
-    if (isset($_POST['mapply_api_key'])){
-      save_mapply_api(sanitize_text_field($_POST['mapply_api_key']));
-    }
-
-    // Check if the mapply link was posted
-    if (isset($_POST['mapply_link'])){
-      save_mapply_link($_POST['mapply_link']);
-    }
-
-    // redirect
-    wp_redirect(  admin_url( 'admin.php?page=mapply.php_/mapply.php' ) );
-    exit;
-  }
-}
-
-
-
+// Create the admin menu
 function mapply_create_menu() {
 
   //create new top-level menu
@@ -227,63 +143,9 @@ function register_mysettings() {
 
 }
 
-// Build the settings page
-function mapply_settings_page() {
-  $default_link = "<a href='http://mapply.net'>Mapply by Mapply!</a>";
 
-  $image = WP_PLUGIN_URL . '/mapply/images/logo.png';
-  $styles = WP_PLUGIN_URL . '/mapply/css/mapply_styles.css';
-
-  $api = get_mapply_api();
-  $gapi = get_google_api();
+// Require the important files
+require("includes/functions.php");
+require("includes/admin_page.php");
 
 ?>
-
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/0.16.1/css/semantic.css"/>
-<link rel="stylesheet" type="text/css" href="<?php echo $styles ?>"/>
-
-<div class="navbar">
-    <a class="logo" href="http://mapply.net" target="_blank"><img src="https://app.mapply.net/assets/imgs/logo.png" width="150"></a>
-</div>
-
-<div class="wrap ui segment purple">
-
-<img src="<?php echo $image ?>" width="150">
-<hr>
-<div class="instructions">
-  <p><b>Step 1</b> - First we'll need to have a Mapply account. If you don't have one already, you can sign up for a <a href="http://www.mapply.net">free 30 day trial here</a>! :-)</p>
-  <p><b>Step 2</b> - Once you're signed up and inside your Mapply account, navigate to the <a href="#">API setup page</a> to grap your Mapply and Google Map API keys to populate the fields below.</p>
-  <p><b>Step 3</b> - Once you have all of your <a href="http://www.app.mapply.net">stores setup</a> in your Mapply account, you can insert your map on any page by using the <b>[mapply]</b> shortcode.</p>
-</div>
-
-<form method="post" action="admin-post.php">
-
-    <?php settings_fields( 'baw-settings-group' ); ?>
-    <?php do_settings_sections( 'baw-settings-group' ); ?>
-
-    <input type="hidden" name="action" value="mapply_api_keys" />
-    <input id="mapply_link_box" style="display:none" type="text" name="mapply_link" value="<?php echo $default_link ?>" />
-    <table class="form-table">
-        <tr valign="top">
-        <th scope="row">Mapply API key</th>
-        <td><input id="mapply_api_box" type="text" name="mapply_api_key" class="ui input" value="<?php echo $api ?>" /></td>
-        </tr>
-
-
-
-        <tr valign="top">
-
-        <th scope="row">Google API key</th>
-
-        <td><input type="text" name="google_api_key" class="ui input" value="<?php echo $gapi ?>" /></td>
-
-        </tr>
-
-    </table>
-
-    <?php submit_button(); ?>
-
-</form>
-</div>
-
-<?php } ?>
